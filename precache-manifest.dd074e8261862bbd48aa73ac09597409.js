20fd2a4bfccfa2b61d5278bff5be795f15caaf7b
VottusCode <vottus@vott.us>
2020-10-04 11:30:49 +0200

    Updates

---
+++ precache-manifest.dd074e8261862bbd48aa73ac09597409.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "220735779bb9745865be0f8be389223a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0317e95c3793a4eed7c6",
+    "url": "/static/css/2.c4f291ab.chunk.css"
+  },
+  {
+    "revision": "ee52f37584b3b4fcc2b5",
+    "url": "/static/css/main.98a6389d.chunk.css"
+  },
+  {
+    "revision": "0317e95c3793a4eed7c6",
+    "url": "/static/js/2.25fe1c76.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.25fe1c76.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee52f37584b3b4fcc2b5",
+    "url": "/static/js/main.2f5ef989.chunk.js"
+  },
+  {
+    "revision": "e7a736b139048ee185fe",
+    "url": "/static/js/runtime-main.c7ffeb3e.js"
+  }
+]);
